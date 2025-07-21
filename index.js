e7a0e3e9d80fb62e4b87390ed7127f38ec0fd800
SheikhSalman <assalmanmuhammad@gmail.com>
2025-07-21 12:38:59 +0600

    change repo

---
+++ index.js
@@ -0,0 +1,140 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.dclhmji.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const itemsCollection = client.db('lost-found').collection('items')
+    const recoveredCollection = client.db('lost-found').collection('recovered')
+
+
+    // add item
+    app.post('/additem', async (req, res) => {
+      const data = req.body
+      const result = await itemsCollection.insertOne(data)
+      res.send(result)
+    })
+    // get all item & search
+    app.get('/allitem', async (req, res) => {
+      const { searchParams } = req.query
+      // console.log(searchParams);
+      // search based on title and location
+      let query = {}
+      if (searchParams) {
+
+         query = {
+          $or:
+          [
+             { title: { $regex: searchParams, $options: "i" } },
+
+           { location: { $regex: searchParams, $options: "i" } }
+          ]
+        }  
+
+      }
+
+      const result = await itemsCollection.find(query).toArray()
+      res.send(result)
+    })
+    // veiw details
+    app.get('/details/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      const result = await itemsCollection.findOne(filter)
+      res.send(result)
+    })
+    // my items
+    app.get('/myitem/:email', async (req, res) => {
+      const email = req.params.email
+      const filter = { email: email }
+      const result = await itemsCollection.find(filter).toArray()
+      res.send(result)
+    })
+    // update item
+    app.put('/myitem/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updatedItem = req.body
+      const updatedDoc = {
+        $set: updatedItem
+      }
+      const result = await itemsCollection.updateOne(filter, updatedDoc, options)
+      res.send(result)
+    })
+
+    // delete item
+    app.delete('/myitem/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await itemsCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    // recovered items
+    app.post('/recovered/:itemId', async (req, res) => {
+      const id = req.params.itemId
+      const data = req.body
+      const result = await recoveredCollection.insertOne(data)
+
+      if (result.acknowledged) {
+        await itemsCollection.updateOne(
+          { _id: new ObjectId(id) },
+          {
+            $set: { status: "recovered" }
+          }
+        )
+      }
+
+      res.send(result)
+    })
+
+    // logged in user recovered item
+    app.get('/recovered/:email', async (req, res) => {
+      const email = req.params.email
+      const filter = { recoveredemail: email }
+      const result = await recoveredCollection.find(filter).toArray()
+      res.send(result)
+    })
+
+    // latesst items sorted by date
+    app.get('/latest', async (req, res) => {
+      const result = await itemsCollection.find().sort({ date: -1 }).limit(6).toArray()
+      res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('finnd and lost')
+})
+
+app.listen(port, () => {
+  console.log(`my server is running on port ${port}`);
+})
